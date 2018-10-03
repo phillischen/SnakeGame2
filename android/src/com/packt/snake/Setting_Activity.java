7f1434ecd71d5966fd0b46bf9522c30d25374d5f
luohuizhe <38150181+luohuizhe@users.noreply.github.com>
2018-10-03 17:21:52 +1000

    setting interface

--- android/src/com/packt/snake/Setting_Activity.java
+++ android/src/com/packt/snake/Setting_Activity.java
@@ -5,13 +5,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Switch;
 
 public class Setting_Activity extends AppCompatActivity {
-    private Button backbutton;
+    private Button backbutton,submitButton;
+    private EditText usernameText;
+    private String username = "player";
     private RadioGroup skins;
+    private RadioGroup controls;
+    private Switch adsswitch;
     private int skinno;
+    private int controlno;
+    private boolean adsOff = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,16 @@ public class Setting_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_setting_);
 
         getSupportActionBar().hide();
+
+        adsswitch = (Switch) findViewById(R.id.switch_noads);
+        adsswitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,
+                                         boolean isChecked) {
+                adsOff = isChecked;
+            }
+        });
+
 
         backbutton = findViewById(R.id.back_botton);
         backbutton.setOnClickListener(new View.OnClickListener() {
@@ -28,6 +47,17 @@ public class Setting_Activity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        usernameText = findViewById(R.id.text_name);
+
+        submitButton = findViewById(R.id.submit_button);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                username = usernameText.getText().toString();
+            }
+        });
+
 
         skins = findViewById(R.id.skingroup);
         skins.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
@@ -60,8 +90,34 @@ public class Setting_Activity extends AppCompatActivity {
 
         });
 
-    }
 
+        controls = findViewById(R.id.controlgroup);
+        controls.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
+
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.radio_gravity:
+                        controlno = 3;
+                        break;
+                    case R.id.radio_handle:
+                        controlno = 2;
+                        break;
+                    case R.id.radio_touch:
+                        controlno = 1;
+                        break;
+                    default:
+                        controlno = 1;
+                        break;
+                }
+            }
+
+        });
+
+
+
+
+
+    }
 
 
 }
