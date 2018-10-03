c2e04fb2896eadcda57c4b3cae74e3a46c3bd99b
luohuizhe <38150181+luohuizhe@users.noreply.github.com>
2018-10-03 16:12:03 +1000

    skin interface

--- android/src/com/packt/snake/Setting_Activity.java
+++ android/src/com/packt/snake/Setting_Activity.java
@@ -5,9 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class Setting_Activity extends AppCompatActivity {
     private Button backbutton;
+    private RadioGroup skins;
+    private int skinno;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,5 +28,40 @@ public class Setting_Activity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        skins = findViewById(R.id.skingroup);
+        skins.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
+
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.radio_Skin1:
+                        skinno = 1;
+                        break;
+                    case R.id.radio_Skin2:
+                        skinno = 2;
+                        break;
+                    case R.id.radio_Skin3:
+                        skinno = 3;
+                        break;
+                    case R.id.radio_Skin4:
+                        skinno = 4;
+                        break;
+                    case R.id.radio_Skin5:
+                        skinno = 5;
+                        break;
+                    case R.id.radio_Skin6:
+                        skinno = 6;
+                        break;
+                    default:
+                        skinno = 1;
+                        break;
+                }
+            }
+
+        });
+
     }
+
+
+
 }
