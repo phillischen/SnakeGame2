44d0ff2678fbf377fa67144a0dd5037bfb4edb24
TenglunTan <tenglunt@gmail.com>
2018-10-20 22:23:35 +1100

    FB

--- android/src/com/packt/snake/Score_Activity.java
+++ android/src/com/packt/snake/Score_Activity.java
@@ -12,20 +12,30 @@ import android.widget.ImageView;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.model.SharePhoto;
+import com.facebook.share.widget.ShareDialog;
+
 import java.util.Map;
 
 public class Score_Activity extends AppCompatActivity {
     private Button shareButton,restartButton, shareImageButton;
+    public static final int REQUEST_VIDEO_CODE = 1000;
     private TextView scoreText;
     private MyAssetsManager myAm;
     private SocketConnect myConnect;
     private String score = "";
     private ImageView imageView;
     private View main;
+    CallbackManager callbackManager;
+    ShareDialog shareDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(this.getApplication());
         setContentView(R.layout.activity_score_);
         main = findViewById(R.id.main);
 
@@ -48,6 +58,18 @@ public class Score_Activity extends AppCompatActivity {
         setupButtons();
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode == RESULT_OK){
+            if(requestCode == REQUEST_VIDEO_CODE){
+                Uri selectedPhoto = data.getData();
+                SharePhoto photo = new SharePhoto.Builder()
+                        .setImageUrl(selectedPhoto)
+                        .build();
+            }
+        }
+    }
+
     private void setupButtons(){
         shareButton = findViewById(R.id.share_button);
         shareButton.setOnClickListener(new View.OnClickListener() {
@@ -65,17 +87,52 @@ public class Score_Activity extends AppCompatActivity {
             }
         });
 
-        imageView = (ImageView) findViewById(R.id.imageView);
+//        imageView = (ImageView) findViewById(R.id.imageView);
+//        shareImageButton = findViewById(R.id.share_image_button);
+//        shareImageButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Bitmap b = Screenshot.takescreenshotRootView(imageView);
+//                imageView.setImageBitmap(b);
+//                main.setBackgroundColor(Color.parseColor("#999999"));
+//
+//            }
+//        });
+
+        callbackManager = CallbackManager.Factory.create();
+        shareDialog = new ShareDialog(this);
+
+
+//        shareImageButton = findViewById(R.id.share_image_button);
+//        shareImageButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                ShareLinkContent linkContent = new ShareLinkContent.Builder()
+//                        .setQuote("I have got a super high Score on this Slither.io game!")
+//                        .setContentUrl(Uri.parse("http://slither.io/"))
+//                        .build();
+//                if(ShareDialog.canShow(ShareLinkContent.class)){
+//                    shareDialog.show(linkContent);
+//                }
+//
+//
+//            }
+//        });
+
+
         shareImageButton = findViewById(R.id.share_image_button);
         shareImageButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                Bitmap b = Screenshot.takescreenshotRootView(imageView);
-                imageView.setImageBitmap(b);
-                main.setBackgroundColor(Color.parseColor("#999999"));
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"Select video"),REQUEST_VIDEO_CODE);
 
             }
         });
+
 
         restartButton = findViewById(R.id.restart_button);
         restartButton.setOnClickListener(new View.OnClickListener() {
